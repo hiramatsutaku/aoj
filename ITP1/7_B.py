99ca1a36f777bac4dc9e7848d0fc69f4eb40e37b
hiramatsutaku <hiramatsutaku@gmail.com>
2020-03-04 01:15:42 +0900

    Fix topic#7

--- ITP1/7_B.py
+++ ITP1/7_B.py
@@ -1,3 +1,11 @@
+while True:
+    n, x = map(int, input().split())
+    if n == 0 and x == 0:
+        break
+    count = 0
+    for a in range(1, n + 1):
+        for b in range(a + 1, n + 1):
+            c = x - a - b
             if b < c <= n:
                 count += 1
     print(count)
